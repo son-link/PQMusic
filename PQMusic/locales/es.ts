a957be24ee50170cf271a05cbf5da2bb14a08551
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2022-05-07 08:51:50 +0200

    Translation updated

--- PQMusic/locales/es.ts
+++ PQMusic/locales/es.ts
@@ -4,12 +4,12 @@
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../ui/Ui_add_url_dialog.py" line="52"/>
+        <location filename="../ui/Ui_add_url_dialog.py" line="53"/>
         <source>Add URL(s)</source>
         <translation>Añadir URL(s)</translation>
     </message>
     <message>
-        <location filename="../ui/Ui_add_url_dialog.py" line="53"/>
+        <location filename="../ui/Ui_add_url_dialog.py" line="54"/>
         <source>Write one URL peer line</source>
         <translation>Escribe una url por linea</translation>
     </message>
@@ -17,27 +17,27 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../__init__.py" line="55"/>
+        <location filename="../__init__.py" line="60"/>
         <source>Add file(s)</source>
         <translation>Añadir fichero(s)</translation>
     </message>
     <message>
-        <location filename="../__init__.py" line="63"/>
+        <location filename="../__init__.py" line="68"/>
         <source>Add folder</source>
         <translation>Añadir carpeta</translation>
     </message>
     <message>
-        <location filename="../__init__.py" line="89"/>
+        <location filename="../__init__.py" line="166"/>
         <source>Select file to open</source>
         <translation>Seleccionar fichero a abrir</translation>
     </message>
     <message>
-        <location filename="../__init__.py" line="89"/>
+        <location filename="../__init__.py" line="166"/>
         <source>Audio (*.mp3 *.ogg *.opus *.aac *.m4a *.flac *.wav)</source>
         <translation>Audio (*.mp3 *.ogg *.opus *.aac *.m4a *.flac *.wav)</translation>
     </message>
     <message>
-        <location filename="../__init__.py" line="108"/>
+        <location filename="../__init__.py" line="151"/>
         <source>Select folder</source>
         <translation>Seleccionar carpeta</translation>
     </message>
@@ -57,14 +57,54 @@
         <translation>Album</translation>
     </message>
     <message>
-        <location filename="../player.py" line="226"/>
+        <location filename="../player.py" line="183"/>
         <source>Unknown</source>
         <translation>Desconocido</translation>
     </message>
     <message>
-        <location filename="../__init__.py" line="71"/>
+        <location filename="../__init__.py" line="76"/>
         <source>Add URL</source>
         <translation>Añadir URL</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="109"/>
+        <source>Play/Pause</source>
+        <translation>Reproducir/Pausar</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="117"/>
+        <source>Previous track</source>
+        <translation>Pista anterior</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="125"/>
+        <source>Next track</source>
+        <translation>Siguiente pista</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="133"/>
+        <source>Quit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../player.py" line="208"/>
+        <source>Select playlist to open</source>
+        <translation>Selecciona la lista de reproducción a abrir</translation>
+    </message>
+    <message>
+        <location filename="../player.py" line="328"/>
+        <source>Playlists (*.m3u *.m3u8)</source>
+        <translation>Lista de reprución (*.m3u *.m3u8)</translation>
+    </message>
+    <message>
+        <location filename="../player.py" line="328"/>
+        <source>Save the playlist</source>
+        <translation>Guardar lista de reproducción</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="84"/>
+        <source>Open playlist</source>
+        <translation>Abrir lista de reproducción</translation>
     </message>
 </context>
 </TS>
