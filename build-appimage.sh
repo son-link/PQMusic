2d53a4b2807245259054ebac3635953ef867af30
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2022-05-08 16:57:25 +0200

    Testing the GitHub workflow for deploying AppImages

--- build-appimage.sh
+++ build-appimage.sh
@@ -4,23 +4,25 @@
 rm -r AppDir
 
 # Grab AppImageTools
+ARCH=$(uname -m)
 wget -nc "https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh"
-wget -nc "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
-wget -nc "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
-chmod +x linuxdeploy-x86_64.AppImage linuxdeploy-plugin-conda.sh appimagetool-x86_64.AppImage
+wget -nc "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${ARCH}.AppImage"
+wget -nc "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-${ARCH}.AppImage"
+chmod +x linuxdeploy-${ARCH}.AppImage linuxdeploy-plugin-conda.sh appimagetool-${ARCH}.AppImage
 
 # Install App
 
 # Set Environment
 export CONDA_CHANNELS='local;conda-forge'
-export PIP_REQUIREMENTS='pyqt5 mutagen .'
+export PIP_REQUIREMENTS='pyqt5 mutagen python-magic requests .'
 export PIP_WORKDIR="$REPO_ROOT"
-
+install -Dm644 bin/io.sonlink.pqmusic.png AppDir/usr/share/icons/pqmusic.png
+install -Dm644 bin/io.sonlink.pqmusic.appdata.xml AppDir/usr/share/metainfo/io.sonlink.pqmusic.appdata.xml
 # Deploy
 ./linuxdeploy-x86_64.AppImage \
    --appdir AppDir \
-    -i bin/io.sonlink.minimalplayer.png \
-    -d bin/io.sonlink.minimalplayer.desktop \
+    -i bin/io.sonlink.pqmusic.png \
+    -d bin/io.sonlink.pqmusic.desktop \
     --plugin conda \
     --custom-apprun bin/AppRun.sh \
     --output appimage
